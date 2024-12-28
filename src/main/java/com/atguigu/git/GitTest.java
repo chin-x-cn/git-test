4bb1a16c1227684199152e0a3ed73c8d75a9f43e
Chen <Chinx.cn@gmail.com>
2024-12-28 15:00:22 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,14 @@
+package com.atguigu.git;
+
+/**
+ * @Program: git-test
+ * @Description:
+ * @Author: Chen
+ * @Create: 2024-12-28 14:59
+ **/
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+        System.out.println("hello git2");
+    }
+}
